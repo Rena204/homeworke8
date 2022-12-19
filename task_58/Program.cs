862429abf35f928132ffcce3da9f6a92b4c11f31
rena <rena2004.rr@gmail.com>
2022-12-19 19:32:11 +0400

    homeworke8

---
+++ task_58/Program.cs
@@ -0,0 +1,56 @@
+﻿int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5); //Для увеличения размера чисел в матрицах поменять число 5 на большее
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("размерность матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+
+
+Console.WriteLine("Матрица - A");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица - B");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц A*B");
+PrintArray(matrixC);
---
+++ task_60/Program.cs
@@ -0,0 +1,37 @@
+﻿int[,,] print = new int[2, 2, 2];
+
+void Print(int[,,] arr)
+{
+    for (int i = 0; i < print.GetLength(0); i++)
+    {
+        for (int j = 0; j < print.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < print.GetLength(2); k++)
+            {
+                Console.Write($"{print[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+
+void Array(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+
+Array(print);
+Print(print);
